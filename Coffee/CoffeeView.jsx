1007190847c7b7a693308938aa9e46d1b5cd022d
12Topre <114716091+12Topre@users.noreply.github.com>
2023-02-16 16:03:03 -0500

    Add files via upload

---
+++ Coffee/CoffeeView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CoffeeView from './CoffeeView';
+
+function App() {
+  return (
+    <div className="App">
+      <CoffeeView />
+    </div>
+  );
+}
+
+export default App;
